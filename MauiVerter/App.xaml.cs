d321220e32a316d01ef34bfdf57b09a4f45fcd7f
Donghwi_Park <gptnc5100@gmail.com>
2024-10-12 10:53:50 +0200

    made a basic Top UI.

--- MauiVerter/App.xaml.cs
+++ MauiVerter/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiVerter
+﻿using MauiVerter.MVVM.Views;
+
+namespace MauiVerter
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MenuView();
         }
     }
 }
