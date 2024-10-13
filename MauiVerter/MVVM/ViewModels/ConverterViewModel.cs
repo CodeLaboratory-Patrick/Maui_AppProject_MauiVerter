526f115ed03c7aa68a1302e0536993e8322509ae
Donghwi_Park <gptnc5100@gmail.com>
2024-10-13 16:57:21 +0200

    Filling the collections Measurement Units. (+ installed Nugetpackage : UnitsNet)

--- MauiVerter/MVVM/ViewModels/ConverterViewModel.cs
+++ MauiVerter/MVVM/ViewModels/ConverterViewModel.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitsNet;
 
 namespace MauiVerter.MVVM.ViewModels
 {
-    internal class ConverterViewModel
+    public class ConverterViewModel
     {
+        public string QuantityName {  get; set; }
+        public ObservableCollection<string> FromMeasure {  get; set; }
+        public ObservableCollection<string> ToMeasures { get; set; }
+        public string CurrentFromMeasure { get; set; }
+        public string CurrentToMeasure { get; set; }
+
+        public ConverterViewModel() 
+        {
+            QuantityName = "Length";
+            FromMeasure = LoadMeasures();
+            ToMeasures = LoadMeasures();
+            CurrentFromMeasure = "Meter";
+            CurrentToMeasure = "Centimeter";
+        }
+        private ObservableCollection<string> LoadMeasures()
+        {
+            var types = Quantity.Infos
+                .FirstOrDefault(x => x.Name == QuantityName)
+                .UnitInfos
+                .Select(u => u.Name)
+                .ToList();
+            return new ObservableCollection<string>(types);
+        }
     }
 }
